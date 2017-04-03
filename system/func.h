29fe99d267dc4b9e108e3889ae46ba3a3e7e80d5
zhangbin0027 <1023646124@qq.com>
2017-04-03 21:16:09 +0800

    system调用

---
+++ system/func.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <stdlib.h>
---
+++ system/system.c
@@ -0,0 +1,10 @@
+#include "func.h"
+
+int main()
+{
+	printf("I will system\n");
+	system("cat system.c");
+	printf("I see you\n");
+	return 0;
+}
+
