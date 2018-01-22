336fe4fa8b98b940e9c9ae0a432d2994da4c9756
zhangbin0027 <1023646124@qq.com>
2018-01-22 16:15:07 +0800

    读写文件

---
+++ fopen/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc,char *argv[]){
+	if(argc!=2){
+		printf("error args\n");
+		return -1;
+	}
+	FILE *fp;
+	fp=fopen(argv[1],"ab+");
+	if(NULL==fp){
+		perror("fopen");
+		return -1;
+	}
+	char buf[128]={0};
+	int ret;
+//	ret=fread(buf,sizeof(char),sizeof(buf),fp);
+//	if(ret<=0){
+//		perror("fread");
+//		return -1;
+//	}
+//	printf("ret=%d,buf=%s\n",ret,buf);
+	strcpy(buf,"how are you");
+	ret=fwrite(buf,sizeof(char),strlen(buf),fp);
+	fclose(fp);
+	return 0;
+}
